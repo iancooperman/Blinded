e841bfd45228bf7b96c6977f79db60f4b7706b6a
Ian Cooperman <i.perry.leonard.c@gmail.com>
2019-02-03 11:48:26 -0800

    Fixed bugs relating to movement code.
    Changed how the player detects obstacles before moving to ObstacleArea instead of Raycast2D.

--- Assets/Player/Scripts/GridMovement.cs
+++ Assets/Player/Scripts/GridMovement.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class GridMovement : MonoBehaviour
 {
 
-    public float unitsPerKeypress = 2f;
+    public float unitsPerKeypress = 1f;
 
     private Rigidbody2D rb;
+    private CircleCollider2D c;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        c = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
@@ -23,43 +25,68 @@ public class GridMovement : MonoBehaviour
 
     void Move() {
         if (Input.GetKeyDown("w")) {
-            // See if there is anything blocking total movement requested.
-            RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.up, unitsPerKeypress);
-
-            // Move only if there is nothing in the way. Same for other if statements.
-            if (hit.collider == null) {
-                rb.MovePosition(rb.position + Vector2.up * unitsPerKeypress);
-            }
+            DetectObstacleAndMove(Vector2.up);
         }
 
         if (Input.GetKeyDown("a")) {
-            // See if there is anything blocking total movement requested.
-            RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.left, unitsPerKeypress);
-            
-            // Move only if there is nothing in the way. Same for other if statements.
-            if (hit.collider == null) {
-                rb.MovePosition(rb.position + Vector2.left * unitsPerKeypress);
-            }
+            DetectObstacleAndMove(Vector2.left);
         }
 
         if (Input.GetKeyDown("s")) {
-            // See if there is anything blocking total movement requested.
-            RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.down, unitsPerKeypress);
-
-            // Move only if there is nothing in the way. Same for other if statements.
-            if (hit.collider == null) {
-                rb.MovePosition(rb.position + Vector2.down * unitsPerKeypress);
-            }
+            DetectObstacleAndMove(Vector2.down);
         }
 
         if (Input.GetKeyDown("d")) {
-            // See if there is anything blocking total movement requested.
-            RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.right, unitsPerKeypress);
-
-            // Move only if there is nothing in the way. Same for other if statements.
-            if (hit.collider == null) {
-                rb.MovePosition(rb.position + Vector2.right * unitsPerKeypress);
-            }
+            DetectObstacleAndMove(Vector2.right);
         }
+    }
+
+    void DetectObstacleAndMove(Vector2 direction) {
+        // // See if there is anything blocking total movement requested.
+        // RaycastHit2D hit = Physics2D.Raycast(rb.position, direction);
+
+        // // Calculate the diameter of the player
+        // float diameter = 2 * c.radius * transform.lossyScale.x;
+        // float radius = c.radius * transform.lossyScale.x;
+
+        // Debug.Log(Mathf.Abs(hit.distance - radius));
+        // Debug.Log(unitsPerKeypress);
+
+        
+
+        // // Move only if there is nothing in the way (the player would logically fit where it's trying to move). Same for other if statements.
+        // if (hit.collider != null) {
+        //     if (Mathf.Abs(hit.distance - radius) > unitsPerKeypress) {
+        //         rb.MovePosition(rb.position + direction * unitsPerKeypress);
+        //     }
+        // }
+
+        // else {
+        //     // What if there's an obstacle that isn't directly in front of the center of the player?
+        //     Collider2D colliderCircle = Physics2D.OverlapCircle(rb.position + direction * unitsPerKeypress, radius);
+
+        //     if (colliderCircle == null) {
+        //         rb.MovePosition(rb.position + direction * unitsPerKeypress);
+        //     }
+
+        // }
+
+        float radius = c.radius * transform.lossyScale.x;
+
+        Vector2 pointA = rb.position + (direction * radius) + (Vector2.Perpendicular(direction) * radius);
+        Vector2 pointB = rb.position + (direction * (unitsPerKeypress + radius)) + (-Vector2.Perpendicular(direction) * radius);
+
+        // Designers must make sure walls and other obstacles are given the layer of obstacle.
+        int ObstacleLayer = LayerMask.GetMask("Obstacle");
+
+        Collider2D overlap = Physics2D.OverlapArea(pointA, pointB, ObstacleLayer);
+
+        Debug.Log(overlap);
+
+        if (overlap == null) {
+            rb.MovePosition(rb.position + direction * unitsPerKeypress);
+        }
+
+        
     }
 }
