08da89bcfadbd6c63eae7b08642489965a4bfd85
Ian Cooperman <i.perry.leonard.c@gmail.com>
2019-05-08 13:30:39 -0700

    Door Script hopefully done

---
+++ Assets/Design Prefabs/Door/DoorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public MeshRenderer meshRenderer;
+    public MeshCollider meshCollider;
+
+    private void Start() {
+
+    }
+
+    public void Close() {
+        meshRenderer.enabled = true;
+        meshCollider.enabled = true;
+    }
+
+    public void Open() {
+        meshRenderer.enabled = false;
+        meshCollider.enabled = false;
+    }
+}
