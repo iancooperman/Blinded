3bc15268b05ffbe96dee5bc7331cdc6a66667f21
Ian Cooperman <i.perry.leonard.c@gmail.com>
2019-05-29 16:50:48 -0700

    Added a Working Dialogue Manager
    Yes, that includes subtitles.

---
+++ Assets/Design Prefabs/Dialogue Manager/DialogueManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text subtitleText;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This should clear the subtitle text
+        if (!audioSource.isPlaying)
+        {
+            subtitleText.text = "";
+        }
+        
+    }
+
+    public void PlayDialogue(AudioClip voiceClip, string subtitle)
+    {
+        audioSource.clip = voiceClip;
+        audioSource.Play();
+        subtitleText.text = subtitle;
+    }
+}
