8352ef1738f4e8fb8825d7d183c1272e5e997617
Ian Cooperman <i.perry.leonard.c@gmail.com>
2019-06-01 12:44:57 -0700

    Fixed a critical oversight in the new dialogue system
    Can now play successive dialogue events without issue.

--- Assets/Design Prefabs/Dialogue Manager/DialogueManager.cs
+++ Assets/Design Prefabs/Dialogue Manager/DialogueManager.cs
@@ -7,12 +7,16 @@ public class DialogueManager : MonoBehaviour
 {
     public Text subtitleText;
 
+
+    private Queue<DialogueClip> dialogues;
     private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        dialogues = new Queue<DialogueClip>();
+        StartCoroutine("PlayDialogue");
     }
 
     // Update is called once per frame
@@ -26,10 +30,38 @@ public class DialogueManager : MonoBehaviour
         
     }
 
-    public void PlayDialogue(AudioClip voiceClip, string subtitle)
+
+    public void AddDialogue(AudioClip voiceClip, string subtitle)
     {
-        audioSource.clip = voiceClip;
-        audioSource.Play();
-        subtitleText.text = subtitle;
+        DialogueClip dialogue = new DialogueClip();
+        dialogue.voiceClip = voiceClip;
+        dialogue.subtitle = subtitle;
+        dialogues.Enqueue(dialogue);
+    }
+
+
+    IEnumerator PlayDialogue()
+    {
+        while (true)
+        {
+            // Don't bother doing anything until there's actually dialogue to do
+            yield return new WaitUntil(() => dialogues.Count > 0);
+
+            // Wait until any current voice clip is done playing
+            yield return new WaitWhile(() => audioSource.isPlaying);
+
+
+            // Play the dialogue
+            DialogueClip dialogue = dialogues.Dequeue();
+            audioSource.clip = dialogue.voiceClip;
+            audioSource.Play();
+            subtitleText.text = dialogue.subtitle;
+        }
     }
 }
+
+struct DialogueClip
+{
+    public AudioClip voiceClip;
+    public string subtitle;
+};
