a70e4839f70a02a12d853620915c0cad594d1dfa
Armando Contreras <vnanoc@hotmail.com>
2019-06-01 16:42:11 -0700

    All maps finished
    -Crude maps finalized, can be polished.

--- Assets/Design Prefabs/Player/Scripts/Respawn.cs
+++ Assets/Design Prefabs/Player/Scripts/Respawn.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    //[SerializeField] private Transform player;
+    [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Player position is: " + GameObject.Find("Player").transform.position);
         //player.transform.position = respawnPoint.transform.position;
         GameObject.Find("Player").transform.position = respawnPoint.transform.position;
     }
