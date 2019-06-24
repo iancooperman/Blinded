88fbba8f22f44718c463a73608a92455198c9e44
Armando Contreras <vnanoc@hotmail.com>
2019-06-23 17:50:41 -0700

    Teleporting/Respawns
    - Added New Script
    -New teleporter*
    -New teleport place

---
+++ Assets/Design Prefabs/Player/Scripts/Teleport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    //public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //player.transform.position = teleportTarget.transform.position;
+        GameObject.Find("Player").transform.position = teleportTarget.transform.position;
+    }
+}
