504b384b42f9c3eb027225b814cd4a0d032e48a9
Ian Cooperman <i.perry.leonard.c@gmail.com>
2019-07-03 21:27:35 -0700

    Began work on turning monster into prefab

---
+++ Assets/Design Prefabs/Monster/Monster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public Transform playerRespawnPoint;
+    //public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //player.transform.position = teleportTarget.transform.position;
+        GameObject.Find("Player").transform.position = playerRespawnPoint.transform.position;
+    }
+}
